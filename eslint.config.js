ed2ae6d3aa0b28e369dbc757661afd06df0f4f78
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-08-22 15:26:12 +0300

    added eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
+
+
+let count = 0;
+const intervalId = setInterval(() => { console.log(count++); }, 1000);
+setTimeout(() => { clearInterval(intervalId); }, 5000);
